d40ceb0a12736c7c0a97be28ddb49029855e5430
unknown <moosafarhana@gmail.com>
2025-07-09 21:42:00 +0200

    ADG with insadeDAFM run1

--- nnunet_mednext/training/network_training/nnUNetTrainer.py
+++ nnunet_mednext/training/network_training/nnUNetTrainer.py
@@ -150,8 +150,8 @@ class nnUNetTrainer(NetworkTrainer):
 
         wandb.login(key="7cf8571ce9a18a2063097f4ec11428ed2ebd3cb7")
         run = wandb.init(
-            project="MedNeXt_InsaneDA_AdamW_M_K5_Fold0",
-            name = f"Epoch_0_15",
+            project="Mednext_ADG_InsaneDAFM_AdamW_S_K3_FoldALL",
+            name = f"Epoch_0_29",
             config={                     
                 "learning_rate": self.initial_lr
             },
